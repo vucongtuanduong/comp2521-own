0c2f11867fdea3bc5509eb7a3d3d1cf5acfe4abf
Duong Vu Cong Tuan <duongvct@duongvct>
2024-01-09 17:33:25 +0700

    task 1 ass1

--- Resources/Assignment/ass1/files/huffman.c
+++ Resources/Assignment/ass1/files/huffman.c
@@ -12,7 +12,27 @@
 
 // Task 1
 void decode(struct huffmanTree *tree, char *encoding, char *outputFilename) {
+    FILE *outputFile = fopen(outputFilename, "w");
+    if (outputFile == NULL) {
+        fprintf(stderr, "Unable to open file %s\n", outputFilename);
+        return;
+    }
 
+    struct huffmanTree *currentNode = tree;
+    for (int i = 0; encoding[i] != '\0'; i++) {
+        if (encoding[i] == '0') {
+            currentNode = currentNode->left;
+        } else if (encoding[i] == '1') {
+            currentNode = currentNode->right;
+        }
+
+        if (currentNode->left == NULL && currentNode->right == NULL) {
+            fprintf(outputFile, "%c", currentNode->token[0]);
+            currentNode = tree;
+        }
+    }
+
+    fclose(outputFile);
 }
 
 // Task 3
